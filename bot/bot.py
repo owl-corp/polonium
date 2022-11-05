1b64e88599eec0c728ec36f5ba09d1cc7a1fba57
Chris Lovering <chris.lovering.95@gmail.com>
2022-11-05 17:29:49 +0000

    Add a healthcheck for postgres on startup

--- bot/bot.py
+++ bot/bot.py
@@ -2,10 +2,15 @@ from pydis_core import BotBase
 from pydis_core.utils import scheduling
 
 from bot import exts
+from bot.settings import Connections
 
 
 class Bot(BotBase):
     """A subclass of `pydis_core.BotBase` that implements bot-specific functions."""
+
+    async def ping_services(self) -> None:
+        """Ensure postgres is available on startup."""
+        await Connections.DB_SESSION().connection()
 
     async def setup_hook(self) -> None:
         """Perform async initialisation method for Discord.py."""
